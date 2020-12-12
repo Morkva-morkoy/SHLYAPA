92aa0b54d081049ff7445f2a848002f7f6d7f70e
SHLYAPA <stepa2429999@gmail.com>
2020-12-13 00:23:32 +0300

    file for functions

--- functions.py
+++ functions.py
@@ -47,3 +47,10 @@ def get_weather():
     observation = mgr.weather_at_place('Saint Petersburg, RU')
     w = observation.weather
     return w.temperature('celsius')
+
+def get_weather1():
+    owm = OWM('aad83395ec9296cd0663d1ac20472eb1')
+    mgr = owm.weather_manager()
+    observation = mgr.weather_at_place('Saint Petersburg, RU')
+    w = observation.weather
+    return w
