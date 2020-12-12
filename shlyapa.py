f9969b29efb478f8bfd50a79145845cd0a0f4869
SHLYAPA <stepa2429999@gmail.com>
2020-12-12 23:03:19 +0300

    executive file

--- shlyapa.py
+++ shlyapa.py
@@ -99,7 +99,7 @@ def execute_cmd(cmd, voice):
     if cmd == 'ctime':
         speak('Сейчас' + str(now.hour) + ':' + str(now.minute))
     if cmd == 'web_search':
-        webbrowser.open_new_tab('https://www.google.com/search?q={}'.format(' '.join(voice.split()[2:])))
+        webbrowser.open_new_tab('https://www.google.com/search?q={}'.format('+'.join(voice.split()[2:])))
     if cmd == 'course_usd':
         speak('{} рублей'.format(convert[0].text))
     if cmd == 'course_eur':
