9ea76855bff71302927e0227f5ffcb390b1655ca
RomanRusinov <you@example.com>
2020-12-17 20:59:27 +0300

    bruh

--- shlyapa.py
+++ shlyapa.py
@@ -14,6 +14,7 @@ from pyowm import OWM
 from pyowm.utils import config
 from pyowm.utils import timestamps
 from covid import Covid
+
 opts = {
     'names': ('шляпа', 'шляпы', 'шляпу'),
     'tbr': ('сколько', 'какое'),
