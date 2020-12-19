98b288679b78da7516f60bb4fda38d802e7a2c92
Morkva-morkoy <stepa2429999@gmail.com>
2020-12-19 20:38:10 +0300

    update function for apps

--- shlyapa.py
+++ shlyapa.py
@@ -17,36 +17,63 @@ from covid import Covid
 import getpass
 
 opts = {
-    'names': ('шляпа', 'шляпы', 'шляпу'),
-    'tbr': ('сколько', 'какое', 'блин'),
-    'cmds': {
-        'ctime': ('сейчас времени', 'сейчас время', 'время', 'времени'),
-        'cdate': ('сегодня число', 'число'),
-        'web_search': ('найди', 'найти'),
-        'course_usd': ('курс доллара', 'доллар в рублях'),
-        'course_eur': ('курс евро', 'евро в рублях'),
-        'apps': ('открой', 'запусти'),
-        'send': ('отправь', 'сообщение'),
-        'note': ('напомни', 'напомни мне'),
-        'note1': ('мои планы', 'что у меня запланировано'),
-        'off': ('выключи', 'выключи компьютер'),
-        'temp': ('температура', 'какая сейчас температура'),
-        'sky': ('осадки', 'какие сейчас осадки'),
-        'wind_speed': ('ветер', 'какая сейчас ветeр', 'скорость ветра'),
-        'if_rain': ('dasdsad', 'сейчас есть дождь'),
-        'humidity': ('влажность', 'какоая сейчас влажность'),
-        'corona': ('коронавирус', 'случаев короновируса')
-    }
+    "names": ("шляпа", "шляпы", "шляпу"),
+    "tbr": ("сколько", "какое", "блин"),
+    "cmds": {
+        "ctime": ("сейчас времени", "сейчас время", "время", "времени"),
+        "cdate": ("сегодня число", "число"),
+        "web_search": ("найди", "найти"),
+        "course_usd": ("курс доллара", "доллар в рублях"),
+        "course_eur": ("курс евро", "евро в рублях"),
+        "apps": ("открой", "запусти"),
+        "send": ("отправь", "сообщение"),
+        "note": ("напомни", "напомни мне"),
+        "note1": ("мои планы", "что у меня запланировано"),
+        "off": ("выключи", "выключи компьютер"),
+        "temp": ("температура", "какая сейчас температура"),
+        "sky": ("осадки", "какие сейчас осадки"),
+        "wind_speed": ("ветер", "какая сейчас ветeр", "скорость ветра"),
+        "if_rain": ("dasdsad", "сейчас есть дождь"),
+        "humidity": ("влажность", "какоая сейчас влажность"),
+        "corona": ("коронавирус", "случаев короновируса"),
+    },
 }
-vk_names = {'dasd': 879796568, 'роме': 617562550, 'мне': 370300823, 'духи': 310799106,
-            'витя': 429372253, 'вове': 97233590, 'арсению': 332716512}
-list_months = ['bruh', 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь',
-               'ноябрь', 'декабрь']
+vk_names = {
+    "dasd": 879796568,
+    "роме": 617562550,
+    "мне": 370300823,
+    "духи": 310799106,
+    "витя": 429372253,
+    "вове": 97233590,
+    "арсению": 332716512,
+}
+list_months = [
+    "bruh",
+    "январь",
+    "февраль",
+    "март",
+    "апрель",
+    "май",
+    "июнь",
+    "июль",
+    "август",
+    "сентябрь",
+    "октябрь",
+    "ноябрь",
+    "декабрь",
+]
 
-counties = {'россии': 'Russia', 'украине': 'Ukraine', 'америке': 'US', 'индии': 'India', 'бразилии': 'Brazil',
-            'франции': 'France', 'великобритании': 'United Kingdom'}
+counties = {
+    "россии": "Russia",
+    "украине": "Ukraine",
+    "америке": "US",
+    "индии": "India",
+    "бразилии": "Brazil",
+    "франции": "France",
+    "великобритании": "United Kingdom",
+}
 
-cities = {'санкт-петербурге': 'Saint Petersburg, RU', 'москве': 'Moscow, RU'}
+cities = {"санкт-петербурге": "Saint Petersburg, RU", "москве": "Moscow, RU"}
 
 r = sr.Recognizer()
 m = sr.Microphone(device_index=0)
@@ -63,129 +90,163 @@ def speak(what):
 
 def callback(recognizer, audio):
     try:
-        voice = recognizer.recognize_google(audio, language='ru-RU').lower()
-        print('[log] Распознано: ' + voice)
-        if voice.startswith(opts['names']):
+        voice = recognizer.recognize_google(audio, language="ru-RU").lower()
+        print("[log] Распознано: " + voice)
+        if voice.startswith(opts["names"]):
             cmd = voice
             cmd = recognize_cmd(cmd)
-            execute_cmd(cmd['cmd'], voice, counties, cities)
+            execute_cmd(cmd["cmd"], voice, counties, cities)
 
     except sr.UnknownValueError:
-        print('[log] Голос не распознан')
+        print("[log] Голос не распознан")
     except sr.RequestError:
-        print('[log] Bruh')
+        print("[log] Bruh")
 
 
 def recognize_cmd(cmd):
-    RC = {'cmd': '', 'percent': 0}
-    for c, v in opts['cmds'].items():
+    RC = {"cmd": "", "percent": 0}
+    for c, v in opts["cmds"].items():
         for i in v:
             vrt = fuzz.ratio(cmd, i)
-            if vrt > RC['percent']:
-                RC['cmd'] = c
-                RC['percent'] = vrt
+            if vrt > RC["percent"]:
+                RC["cmd"] = c
+                RC["percent"] = vrt
     return RC
 
 
-with open('notes.txt', 'r') as file:
+with open("notes.txt", "r") as file:
     text_for_print = file.read()
 
 note_time = functions.note()
 
-time_now = '{}:{}'.format(datetime.datetime.now().hour, datetime.datetime.now().minute)
+time_now = "{}:{}".format(datetime.datetime.now().hour, datetime.datetime.now().minute)
 
-dollar_eur = 'https://www.cbr.ru/'
+dollar_eur = "https://www.cbr.ru/"
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/87.0.4280.66 Safari/537.36'}
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/87.0.4280.66 Safari/537.36"
+}
 
 course_page = requests.get(dollar_eur, headers=headers)
-soup = BeautifulSoup(course_page.content, 'html.parser')
-convert = soup.find_all('div', {'class': 'col-md-2 col-xs-9 _right mono-num'})
+soup = BeautifulSoup(course_page.content, "html.parser")
+convert = soup.find_all("div", {"class": "col-md-2 col-xs-9 _right mono-num"})
+
 
 def execute_cmd(cmd, voice, countries, cities):
+    global USER_NAME, voice_for_apps
     now = datetime.datetime.now()
-    if cmd == 'cdate':
-        speak(str('Сейчас' + str(now.day) + ',' + list_months[now.month]))
-    if cmd == 'ctime':
-        speak('Сейчас' + str(now.hour) + ':' + str(now.minute))
-    if cmd == 'web_search':
-        webbrowser.open_new_tab('https://www.google.com/search?q={}'.format('+'.join(voice.split()[2:])))
-    if cmd == 'course_usd':
-        speak('{} рублей'.format(convert[0].text))
-    if cmd == 'course_eur':
-        speak('{} рублей'.format(convert[2].text))
-    if cmd == 'apps':
+    if cmd == "cdate":
+        speak(str("Сейчас" + str(now.day) + "," + list_months[now.month]))
+    if cmd == "ctime":
+        speak("Сейчас" + str(now.hour) + ":" + str(now.minute))
+    if cmd == "web_search":
+        webbrowser.open_new_tab(
+            "https://www.google.com/search?q={}".format("+".join(voice.split()[2:]))
+        )
+    if cmd == "course_usd":
+        speak("{} рублей".format(convert[0].text))
+    if cmd == "course_eur":
+        speak("{} рублей".format(convert[2].text))
+    if cmd == "apps":
         try:
             voice_for_apps = voice.split()
             voice_for_apps = voice_for_apps[2:]
-            voice_for_apps = ' '.join(voice_for_apps)
+            voice_for_apps = " ".join(voice_for_apps)
             USER_NAME = getpass.getuser()
-            os.startfile('C:\\Users\\%s\\Desktop\\{}'.format(voice_for_apps) %USER_NAME)
+            os.startfile(
+                "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\{}".format(
+                    voice_for_apps
+                )
+            )
         except Exception:
-            print('incorrect shortcut name')
-    if cmd == 'send':
+            try:
+                os.startfile(
+                    "C:\\Users\\%s\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\{}".format(
+                        voice_for_apps
+                    )
+                    % USER_NAME
+                )
+            except Exception:
+                try:
+                    os.startfile(
+                        "C:\\Users\\%s\\Desktop\\{}".format(voice_for_apps) % USER_NAME
+                    )
+                except Exception:
+                    print("incorrect app")
+
+    if cmd == "send":
         try:
             voice_for_vk = voice.split()
             voice_for_id = voice_for_vk[2]
             voice_for_id = vk_names[voice_for_id]
             voice_for_message = voice_for_vk[3:]
-            voice_for_message = ' '.join(voice_for_message)
+            voice_for_message = " ".join(voice_for_message)
             functions.sent(voice_for_id, voice_for_message)
         except Exception:
             pass
-    if cmd == 'note':
+    if cmd == "note":
         voice = voice.split()
         voice_for_note = voice[3:]
-        voice_for_note = ' '.join(voice_for_note)
-        with open('notes.txt', 'w') as file:
+        voice_for_note = " ".join(voice_for_note)
+        with open("notes.txt", "w") as file:
             file.writelines(voice_for_note)
-    if cmd == 'note1':
+    if cmd == "note1":
         functions.note()
-    if cmd == 'off':
-        check = str(input('Вы уверены что хотите завершить работу компьютера? '))
-        if check == 'да':
+    if cmd == "off":
+        check = str(input("Вы уверены что хотите завершить работу компьютера? "))
+        if check == "да":
             os.system("shutdown /p")
         else:
             pass
-    if cmd == 'temp':
+    if cmd == "temp":
         try:
-            print(functions.get_weather(cities[voice.split()[-1]]).temperature('celsius')['temp'])
+            print(
+                functions.get_weather(cities[voice.split()[-1]]).temperature("celsius")[
+                    "temp"
+                ]
+            )
         except KeyError:
-            print('incorrect city')
-    if cmd == 'sky':
+            print("incorrect city")
+    if cmd == "sky":
         try:
             print(functions.get_weather(cities[voice.split()[-1]]).detailed_status)
         except KeyError:
-            print('incorrect city')
-    if cmd == 'wind':
+            print("incorrect city")
+    if cmd == "wind":
         try:
             print(functions.get_weather(cities[voice.split()[-1]]).wind())
         except KeyError:
-            print('incorrect city')
-    if cmd == 'if_rain':
+            print("incorrect city")
+    if cmd == "if_rain":
         try:
             print(functions.get_weather(cities[voice.split()[-1]]).rain)
         except KeyError:
-            print('incorrect city')
-    if cmd == 'humidity':
+            print("incorrect city")
+    if cmd == "humidity":
         try:
             print(functions.get_weather(cities[voice.split()[-1]]).humidity)
         except KeyError:
-            print('incorrect city')
-    if cmd == 'corona':
-        covid = Covid(source='worldometers')
+            print("incorrect city")
+    if cmd == "corona":
+        covid = Covid(source="worldometers")
         action = covid.get_status_by_country_name(countries[voice.split()[-1]])
-        print('В {} {} новых случаев за сегодня'.format(voice.split()[-1], action['new_cases']))
+        print(
+            "В {} {} новых случаев за сегодня".format(
+                voice.split()[-1], action["new_cases"]
+            )
+        )
+
 
 stop_listening = r.listen_in_background(m, callback)
 
 while True:
     time.sleep(0.1)
-    time_now = '{}:{}'.format(datetime.datetime.now().hour, datetime.datetime.now().minute)
+    time_now = "{}:{}".format(
+        datetime.datetime.now().hour, datetime.datetime.now().minute
+    )
     if time_now == note_time:
-        print('Вам пора {}'.format(' '.join(text_for_print.split()[0:-2])))
-        speak('Вам пора {}'.format(' '.join(text_for_print.split()[0:-2])))
-        open('notes.txt', 'w').close()
+        print("Вам пора {}".format(" ".join(text_for_print.split()[0:-2])))
+        speak("Вам пора {}".format(" ".join(text_for_print.split()[0:-2])))
+        open("notes.txt", "w").close()
         time.sleep(60)
